e6e802c552ce46b082be4556cb67502c4076eccd
Fernando Faria <85416605+fmfaria@users.noreply.github.com>
2024-09-20 20:49:53 +0100

    Criar a classe Conversoes e mover método
    O método Converter Temperatura() foi removido da classse Aritmetrica e foi colocado na classe Conversoes  uma vez que implementa uma operação que não se adequad

--- Calculos/Aritmetica.cs
+++ Calculos/Aritmetica.cs
@@ -3,21 +3,6 @@
     /// </summary>
     internal static class Aritmetica
     {
-        /// <summary>
-		/// Tipo de conversão de temperatura a executar.
-		/// </summary>
-		public enum ConversaoTemperatura
-		{
-			Nulo = 0,
-			CelsiusFahrenheit,
-			FahrenheitCelsius
-		}		
-		
-		/// <summary>
-		/// Operação soma.
-		/// </summary>
-		/// <returns>Retorna a soma de dois números.</returns>
-
 		public static int Somar(int x, int y)
         {
             return x + y;
@@ -34,17 +19,5 @@
 			return x - y;
 		}
 		
-		public static double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
-    {
-        if (conversao == ConversaoTemperatura.CelsiusFahrenheit)
-        {
-            return (temperatura * 1.8000 + 32);
-        }
-        else if (conversao == ConversaoTemperatura.FahrenheitCelsius)
-        {
-            return ((temperatura - 32) / 1.8000);
-        }
 
-        return -1;
-    }
 
---
+++ Calculos/Conversoes.cs
@@ -0,0 +1,47 @@
+namespace Calculos
+{
+	/// <summary>
+    /// Conversão de temperaturas.
+    /// </summary>
+	/// <summary>
+	/// Tipo de conversão de temperatura a executar.
+	/// </summary>
+	
+	public enum ConversaoTemperatura
+		{
+			Nulo = 0,
+			CelsiusFahrenheit,
+			FahrenheitCelsius
+		}		
+		
+		/// <summary>
+		/// Operação soma.
+		/// </summary>
+		/// <returns>Retorna a soma de dois números.</returns>
+	internal static class Conversoes
+	{
+    /// <param name="conversao">A conversão a efetuar.</param>
+    /// <param name="temperatura">A temperatua a converter.</param>
+    /// <returns>Retorna o resultado da conversão da temperatura.</returns>
+
+	public static double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
+    {
+        if (conversao == ConversaoTemperatura.CelsiusFahrenheit)
+        {
+            return (temperatura * 1.8000 + 32);
+        }
+        else if (conversao == ConversaoTemperatura.FahrenheitCelsius)
+        {
+            return ((temperatura - 32) / 1.8000);
+        }
+
+        return -1;
+    }
+
+
+		
+		
+		
+	}
+	
+}
